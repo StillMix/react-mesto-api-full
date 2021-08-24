d415fd2e3ca63f2d78cdaef365f80bc1acb77290
StillMix <stillmixplaygames@gmail.com>
2021-08-24 18:25:13 +0300

    first commit

--- frontend/src/components/main/Main.js
+++ frontend/src/components/main/Main.js
@@ -14,6 +14,7 @@ function Main(props) {
   const [avatar, setAvatar] = React.useState('');
   const history = useHistory();
 
+  console.log(currentUser);
 React.useEffect(() => {
   currentUser && setName(currentUser.name)
   currentUser && setDescription(currentUser.about)
