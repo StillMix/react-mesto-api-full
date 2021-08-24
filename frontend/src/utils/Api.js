103c95579ccd719342619bf9f41a8b3299c5bba8
StillMix <stillmixplaygames@gmail.com>
2021-08-24 19:11:01 +0300

    first commit

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -89,6 +89,7 @@ class Api {
 const api = new Api({
     url: 'http://SMBackendMesto.nomoredomains.rocks',
     headers: {
+      'Accept': 'application/json',
       'Content-Type': 'application/json',
     }
 })
