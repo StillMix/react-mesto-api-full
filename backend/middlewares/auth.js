e3f2935460a89a7c7a79c91a96178ed8a1441033
StillMix <stillmixplaygames@gmail.com>
2021-08-24 13:48:22 +0300

    first commit

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,22 +4,20 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 module.exports.auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-  try {
-    payload = jwt.verify(token, JWT_SECRET);
-  } catch (err) {
+  if (!req.cookies.jwt) {
     next(res.status(401).send({ message: 'Необходима авторизация' }));
-  }
+  } else {
+    const token = req.cookies.jwt;
+    let payload;
 
-  req.user = payload;
-  next();
+    try {
+      payload = jwt.verify(token, JWT_SECRET);
+    } catch (err) {
+      next(res.status(401).send({ message: 'Необходима авторизация' }));
+    }
+
+    req.user = payload;
+
+    next();
+  }
 };
