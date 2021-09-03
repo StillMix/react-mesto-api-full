0ca3f1958d95ff3bc424d1199e976b246d0403d7
StillMix <stillmixplaygames@gmail.com>
2021-09-03 17:05:10 +0300

    first commit

--- backend/models/card.js
+++ backend/models/card.js
@@ -2,6 +2,7 @@
 /* eslint-disable quotes */
 /* eslint-disable quote-props */
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name:
@@ -15,6 +16,10 @@ const cardSchema = new mongoose.Schema({
   {
     type: String,
     required: true,
+    validate: {
+      validator: (ava) => validator.isURL(ava, { require_protocol: true }),
+      message: 'Неверная ссылка',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
