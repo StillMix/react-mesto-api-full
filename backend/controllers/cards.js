e8767c39b3f79325cdd82a9a3dbf2b2e96d9c9c3
StillMix <stillmixplaygames@gmail.com>
2021-09-03 16:36:03 +0300

    first commit

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -52,7 +52,7 @@ module.exports.createCard = (req, res, next) => {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => {
       if (card) {
-        Card.find({}).then((cards) => res.status(200).send({ data: cards }));
+        res.status(200).send({ data: card });
       }
     })
     .catch((err) => {
