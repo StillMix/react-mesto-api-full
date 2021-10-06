b9ef13fc5721f797179136b6174262d19fdbd087
StillMix <stillmixplaygames@gmail.com>
2021-10-06 21:39:47 +0300

    first commit

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -116,7 +116,7 @@ function addCard(card){
   }else{
   console.log(card)
   api.addCard(card.name, card.link).then((data)=>{
-    console.log(cards.lenght)
+    console.log(cards.length)
        if(cards.length === null){
          setCards(data)
        } else{
